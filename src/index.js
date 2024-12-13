f65d3e7a0ee7ac05b5dfc1249c873f7fd9e336bd
Abdulkerim <kerimewa@gmail.com>
2024-12-13 21:53:19 +0300

    add rating component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
+import "./starStyle.css";
+import { renderToReadableStream } from "react-dom/server";
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [moverRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating onSetRating={setMovieRating} />
+      <p>This movie was rated {moverRating} stars.</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
-
